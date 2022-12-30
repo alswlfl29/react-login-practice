adcbccc10751b8c68e209dea1a0e09291ac67f39
Minji Lee <79428205+alswlfl29@users.noreply.github.com>
2022-12-31 02:53:06 +0900

    feat: 로그인 기능 구현

--- login-practice/src/components/Login.js
+++ login-practice/src/components/Login.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 
+//더미데이터
+const User = {
+  email: "test@example.com",
+  password: "test2323@@@",
+};
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+  const [notAllow, setNotAllow] = useState(true);
+
+  //자바스크립트 정규 표현식 사용
+  //이메일 형식 판단(적절한 형식의 이메일인지 확인)
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    const regex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (regex.test(email)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+
+  //비밀번호 형식 판단
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    const regex =
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    if (regex.test(password)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
+  };
+
+  const onClickConfirmButton = () => {
+    if (email === User.email && password === User.password) {
+      alert("로그인 성공!");
+    } else {
+      alert("등록되지 않은 회원입니다!");
+    }
+  };
+
+  useEffect(() => {
+    if (emailValid && pwValid) {
+      setNotAllow(false);
+      return;
+    }
+    setNotAllow(true);
+  }, [emailValid, pwValid]);
+
   return (
     <div className="page">
       <div className="titleWrap">이메일과 비밀번호를 입력해주세요.</div>
@@ -9,25 +63,44 @@ function Login() {
       <div className="contentWrap">
         <div className="inputTitle">이메일 주소</div>
         <div className="inputWrap">
-          <input className="input" placeholder="test@gmail.com" />
+          <input
+            type="text"
+            className="input"
+            placeholder="test@gmail.com"
+            value={email}
+            onChange={handleEmail} //이 부분 사용하지 않으면 아무리 해도 안바뀜
+          />
         </div>
-        <div className="errorMessageWrap">올바른 이메일을 입력해주세요!</div>
+        <div className="errorMessageWrap">
+          {!emailValid && email.length > 0 && (
+            <div>올바른 이메일을 입력해주세요!</div>
+          )}
+        </div>
         <div style={{ marginTop: "26px" }} className="inputTitle">
           비밀번호
         </div>
         <div className="inputWrap">
           <input
+            type="password"
             className="input"
             placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+            value={password}
+            onChange={handlePassword}
           />
         </div>
         <div className="errorMessageWrap">
-          영문, 숫자, 특수문자 포함 8자 이상 입력해주세요!
+          {!pwValid && password.length > 0 && (
+            <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요!</div>
+          )}
         </div>
       </div>
 
       <div>
-        <button disabled={true} className="bottomButton">
+        <button
+          onClick={onClickConfirmButton}
+          disabled={notAllow}
+          className="bottomButton"
+        >
           확인
         </button>
       </div>
